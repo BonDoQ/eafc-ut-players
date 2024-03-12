7f1fd8d1a40ad9e35e3a29082314aed347b22967
BonDoQ <ahmed@bondoq.me>
2024-03-12 16:32:08 +0100

    Init: Directus

---
+++ config.js
@@ -0,0 +1,24 @@
+module.exports = function (env) {
+  return {
+    // Railway inputs
+    ADMIN_EMAIL: env.ADMIN_EMAIL,
+    ADMIN_PASSWORD: env.ADMIN_PASSWORD,
+    KEY: env.KEY,
+    SECRET: env.SECRET,
+
+    // Reference: https://docs.railway.app/deploy/exposing-your-app
+    PORT: env.PORT,
+
+    // Reference: https://docs.railway.app/develop/variables#railway-provided-variables
+    PUBLIC_URL: env.RAILWAY_PUBLIC_DOMAIN,
+
+    // Database variables from Railway PostgreSQL Plugin
+    // Reference: https://docs.railway.app/plugins/postgresql
+    DB_CLIENT: "pg",
+    DB_HOST: env.PGPRIVATEHOST,
+    DB_PASSWORD: env.PGPASSWORD,
+    DB_USER: env.PGUSER,
+    DB_PORT: env.PGPORT,
+    DB_DATABASE: env.PGDATABASE,
+  };
+};
