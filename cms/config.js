903fd22fc6fef37842311b3b9aa51512fe4603b1
Ahmed ElSawaf <ahmed@bondoq.me>
2024-03-24 09:54:17 +0100

    feat: use R2 for file storage (#15)

--- cms/config.js
+++ cms/config.js
@@ -20,5 +20,15 @@ module.exports = function (env) {
     DB_USER: env.PGUSER,
     DB_PORT: env.PGPORT,
     DB_DATABASE: env.PGDATABASE,
+
+    // Storage Location
+    STORAGE_LOCATIONS: process.env.STORAGE_LOCATIONS,
+    STORAGE_R2_DRIVER: process.env.STORAGE_R2_DRIVER,
+    STORAGE_R2_KEY: process.env.STORAGE_R2_KEY,
+    STORAGE_R2_SECRET: process.env.STORAGE_R2_SECRET,
+    STORAGE_R2_ENDPOINT: process.env.STORAGE_R2_ENDPOINT,
+    STORAGE_R2_BUCKET: process.env.STORAGE_R2_BUCKET,
+    STORAGE_R2_REGION: process.env.STORAGE_R2_REGION,
+    STORAGE_R2_ROOT: process.env.STORAGE_R2_ROOT,
   };
 };
